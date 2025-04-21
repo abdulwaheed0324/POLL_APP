065f67f7db67b05205e81784888d950bb37be69f
Talha Jawaid <talhajawaid601@gmail.com>
2025-04-21 13:06:01 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,116 +1,216 @@
-const $ = (s) => document.querySelector(s);
-const progressBar = $(".progress-bar"), progressText = $(".progress-text");
-const startBtn = $(".start"), numQuestions = $("#num-questions"),
-  category = $("#category"), difficulty = $("#difficulty"),
-  timePerQuestion = $("#time"), quiz = $(".quiz"),
-  startScreen = $(".start-screen"), submitBtn = $(".submit"),
-  nextBtn = $(".next"), endScreen = $(".end-screen"),
-  finalScore = $(".final-score"), totalScore = $(".total-score"),
-  restartBtn = $(".restart");
+const progressBar = document.querySelector(".progress-bar"),
+  progressText = document.querySelector(".progress-text");
 
-let questions = [], time = 30, score = 0, currentQuestion, timer;
-
-const progress = (val) => {
-  const pct = (val / time) * 100;
-  progressBar.style.width = `${pct}%`;
-  progressText.innerHTML = `${val}`;
+const progress = (value) => {
+  const percentage = (value / time) * 100;
+  progressBar.style.width = ${percentage}%;
+  progressText.innerHTML = ${value};
 };
 
-const loadingAnimation = () => {
-  startBtn.innerHTML = "Loading";
-  const int = setInterval(() => startBtn.innerHTML.length === 10 ? startBtn.innerHTML = "Loading" : startBtn.innerHTML += ".", 500);
-};
+const startBtn = document.querySelector(".start"),
+  numQuestions = document.querySelector("#num-questions"),
+  category = document.querySelector("#category"),
+  difficulty = document.querySelector("#difficulty"),
+  timePerQuestion = document.querySelector("#time"),
+  quiz = document.querySelector(".quiz"),
+  startScreen = document.querySelector(".start-screen");
 
-const playAudio = (src) => new Audio(src).play();
+let questions = [],
+  time = 30,
+  score = 0,
+  currentQuestion,
+  timer;
 
 const startQuiz = () => {
-  const url = `https://opentdb.com/api.php?amount=${numQuestions.value}&category=${category.value}&difficulty=${difficulty.value}&type=multiple`;
+  const num = numQuestions.value,
+    cat = category.value,
+    diff = difficulty.value;
   loadingAnimation();
-  fetch(url).then(res => res.json()).then(data => {
-    questions = data.results;
-    setTimeout(() => {
-      startScreen.classList.add("hide");
-      quiz.classList.remove("hide");
-      currentQuestion = 1;
-      showQuestion(questions[0]);
-    }, 1000);
-  });
+  const url = https://opentdb.com/api.php?amount=${num}&category=${cat}&difficulty=${diff}&type=multiple;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      questions = data.results;
+      setTimeout(() => {
+        startScreen.classList.add("hide");
+        quiz.classList.remove("hide");
+        currentQuestion = 1;
+        showQuestion(questions[0]);
+      }, 1000);
+    });
 };
 
-const showQuestion = (q) => {
-  const questionText = $(".question"), answersWrapper = $(".answer-wrapper"),
-    questionNumber = $(".number");
-  questionText.innerHTML = q.question;
-  const answers = [...q.incorrect_answers, q.correct_answer.toString()].sort(() => Math.random() - 0.5);
+startBtn.addEventListener("click", startQuiz);
+
+const showQuestion = (question) => {
+  const questionText = document.querySelector(".question"),
+    answersWrapper = document.querySelector(".answer-wrapper");
+  questionNumber = document.querySelector(".number");
+
+  questionText.innerHTML = question.question;
+
+  const answers = [
+    ...question.incorrect_answers,
+    question.correct_answer.toString(),
+  ];
   answersWrapper.innerHTML = "";
-  answers.forEach((a) => answersWrapper.innerHTML += `
-    <div class="answer"><span class="text">${a}</span><span class="checkbox"><i class="fas fa-check"></i></span></div>`);
-  questionNumber.innerHTML = `Question <span class="current">${questions.indexOf(q) + 1}</span><span class="total">/${questions.length}</span>`;
-  document.querySelectorAll(".answer").forEach((a) => {
-    a.addEventListener("click", () => {
-      if (!a.classList.contains("checked")) {
-        document.querySelectorAll(".answer").forEach((x) => x.classList.remove("selected"));
-        a.classList.add("selected");
+  answers.sort(() => Math.random() - 0.5);
+  answers.forEach((answer) => {
+    answersWrapper.innerHTML += 
+                  <div class="answer ">
+            <span class="text">${answer}</span>
+            <span class="checkbox">
+              <i class="fas fa-check"></i>
+            </span>
+          </div>
+        ;
+  });
+
+  questionNumber.innerHTML =  Question <span class="current">${
+    questions.indexOf(question) + 1
+  }</span>
+            <span class="total">/${questions.length}</span>;
+  //add event listener to each answer
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.addEventListener("click", () => {
+      if (!answer.classList.contains("checked")) {
+        answersDiv.forEach((answer) => {
+          answer.classList.remove("selected");
+        });
+        answer.classList.add("selected");
         submitBtn.disabled = false;
       }
     });
   });
+
   time = timePerQuestion.value;
   startTimer(time);
 };
 
-const startTimer = (t) => {
+const startTimer = (time) => {
   timer = setInterval(() => {
-    if (t === 3) playAudio("countdown.mp3");
-    if (t >= 0) {
-      progress(t);
-      t--;
-    } else checkAnswer();
+    if (time === 3) {
+      playAdudio("countdown.mp3");
+    }
+    if (time >= 0) {
+      progress(time);
+      time--;
+    } else {
+      checkAnswer();
+    }
   }, 1000);
 };
 
-const checkAnswer = () => {
-  clearInterval(timer);
-  const selected = $(".answer.selected");
-  const correct = questions[currentQuestion - 1].correct_answer;
-  if (selected) {
-    const answer = selected.querySelector(".text").innerHTML;
-    answer === correct ? (score++, selected.classList.add("correct")) : (selected.classList.add("wrong"));
-  }
-  document.querySelectorAll(".answer").forEach((a) => {
-    if (a.querySelector(".text").innerHTML === correct) a.classList.add("correct");
-    a.classList.add("checked");
-  });
-  submitBtn.style.display = "none";
-  nextBtn.style.display = "block";
+const loadingAnimation = () => {
+  startBtn.innerHTML = "Loading";
+  const loadingInterval = setInterval(() => {
+    if (startBtn.innerHTML.length === 10) {
+      startBtn.innerHTML = "Loading";
+    } else {
+      startBtn.innerHTML += ".";
+    }
+  }, 500);
 };
-
-const nextQuestion = () => currentQuestion < questions.length ? showQuestion(questions[currentQuestion++]) : showScore();
-
-const showScore = () => {
-  endScreen.classList.remove("hide");
-  quiz.classList.add("hide");
-  finalScore.innerHTML = score;
-  totalScore.innerHTML = `/ ${questions.length}`;
-};
-
-const defineProperty = () => {
-  const osccred = document.createElement("div");
-  osccred.innerHTML = "A Project By TALHA JAWAID AND ABDUL WAHEED";
-  Object.assign(osccred.style, {
-    position: "absolute", bottom: "0", right: "0", fontSize: "10px",
-    color: "#ccc", fontFamily: "sans-serif", padding: "5px", background: "#fff",
-    borderTopLeftRadius: "5px", borderBottomRightRadius: "5px", boxShadow: "0 0 5px #ccc"
-  });
+function defineProperty() {
+  var osccred = document.createElement("div");
+  osccred.innerHTML =
+    "A Project By TALHA JAWAID AND ABDUL WAHEED";
+  osccred.style.position = "absolute";
+  osccred.style.bottom = "0";
+  osccred.style.right = "0";
+  osccred.style.fontSize = "10px";
+  osccred.style.color = "#ccc";
+  osccred.style.fontFamily = "sans-serif";
+  osccred.style.padding = "5px";
+  osccred.style.background = "#fff";
+  osccred.style.borderTopLeftRadius = "5px";
+  osccred.style.borderBottomRightRadius = "5px";
+  osccred.style.boxShadow = "0 0 5px #ccc";
   document.body.appendChild(osccred);
-};
+}
 
-startBtn.addEventListener("click", startQuiz);
-submitBtn.addEventListener("click", checkAnswer);
+defineProperty();
+
+const submitBtn = document.querySelector(".submit"),
+  nextBtn = document.querySelector(".next");
+submitBtn.addEventListener("click", () => {
+  checkAnswer();
+});
+
 nextBtn.addEventListener("click", () => {
   nextQuestion();
   submitBtn.style.display = "block";
   nextBtn.style.display = "none";
 });
-restartBtn.addEventListener("click", () => window.location.reload());
-defineProperty();
+
+const checkAnswer = () => {
+  clearInterval(timer);
+  const selectedAnswer = document.querySelector(".answer.selected");
+  if (selectedAnswer) {
+    const answer = selectedAnswer.querySelector(".text").innerHTML;
+    console.log(currentQuestion);
+    if (answer === questions[currentQuestion - 1].correct_answer) {
+      score++;
+      selectedAnswer.classList.add("correct");
+    } else {
+      selectedAnswer.classList.add("wrong");
+      const correctAnswer = document
+        .querySelectorAll(".answer")
+        .forEach((answer) => {
+          if (
+            answer.querySelector(".text").innerHTML ===
+            questions[currentQuestion - 1].correct_answer
+          ) {
+            answer.classList.add("correct");
+          }
+        });
+    }
+  } else {
+    const correctAnswer = document
+      .querySelectorAll(".answer")
+      .forEach((answer) => {
+        if (
+          answer.querySelector(".text").innerHTML ===
+          questions[currentQuestion - 1].correct_answer
+        ) {
+          answer.classList.add("correct");
+        }
+      });
+  }
+  const answersDiv = document.querySelectorAll(".answer");
+  answersDiv.forEach((answer) => {
+    answer.classList.add("checked");
+  });
+
+  submitBtn.style.display = "none";
+  nextBtn.style.display = "block";
+};
+
+const nextQuestion = () => {
+  if (currentQuestion < questions.length) {
+    currentQuestion++;
+    showQuestion(questions[currentQuestion - 1]);
+  } else {
+    showScore();
+  }
+};
+
+const endScreen = document.querySelector(".end-screen"),
+  finalScore = document.querySelector(".final-score"),
+  totalScore = document.querySelector(".total-score");
+const showScore = () => {
+  endScreen.classList.remove("hide");
+  quiz.classList.add("hide");
+  finalScore.innerHTML = score;
+  totalScore.innerHTML = / ${questions.length};
+};
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+const playAdudio = (src) => {
+  const audio = new Audio(src);
+  audio.play();
